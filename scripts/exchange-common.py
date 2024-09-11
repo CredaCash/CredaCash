d1f6a387b2b15a55190f0754713f6f6789496d56
CredaCash <github@credacash.com>
2024-09-11 17:35:03 -0400

    CredaCash v2.0.1
    Updated exchange scripts to increase robustness and support Python v3

--- scripts/exchange-common.py
+++ scripts/exchange-common.py
@@ -36,8 +36,40 @@ BITCOIND = 'Bitcoind'
 ELECTRUM = 'Electrum'
 ELECTRON = 'Electron Cash'
 MINING = 'Mining'
-
 TIMEOUT = 'TIMEOUT'
+
+def ensure_one_instance(id):
+	#print 'script instance id = %s' % id
+
+	if os.name == 'nt':
+		import ctypes
+		windll = ctypes.cdll.LoadLibrary('Kernel32.dll')
+		wincmd = windll.CreateMutexA
+		wincmd.argtypes = [ctypes.c_char_p, ctypes.c_uint32, ctypes.c_char_p]
+		wincmd(None, 0, id.encode())
+		rc = windll.GetLastError()
+		#pprint.pprint(rc)
+		if rc == 0:
+			already_running = False
+		elif rc == 183:
+			already_running = True
+		else:
+			print('\nERROR: unrecognized GetLastError return code', rc)
+			exit()
+	else:
+		import fcntl
+		global flock
+		flock = open('/tmp/' + id, 'wb')
+		try:
+			fcntl.lockf(flock, fcntl.LOCK_EX | fcntl.LOCK_NB)
+			already_running = False
+		except (IOError, OSError):
+			already_running = True
+
+	if already_running:
+		print('\nAn instance of this script appears to be already running.')
+		print('To minimize the chance of double payments, only one instance is allowed.\n')
+		exit()
 
 def do_rpc(s, c, method, params=(), timeout=600, return_timeout=False, needs_wallet=False):
 	if needs_wallet and c.type == ELECTRUM and c.currency == 'btc':
@@ -175,9 +207,338 @@ def parse_config(conf_file, include_mining=True, ForeignClass=Config):
 	Foreign = ForeignClass(c, conf_file, FOREIGN)
 	if include_mining:
 		Mining = Config(c, conf_file, MINING)
-	else:
+	elif MINING in c:
 		del c[MINING]
 	Config.del_excluded_keys(c)
 	if len(c) > 0:
 		print('ERROR: unrecognized elements in configuration file %s:' % conf_file, c)
 		exit()
+
+class ForeignRPC(Config):
+
+	def IsBitcoind(self):
+		return self.type == BITCOIND
+
+	def IsElectrum(self):
+		return self.type == ELECTRUM
+
+	def InvalidTypeError(self):
+		print('ERROR: invalid foreign rpc type', self.type)
+		return None
+
+	def IsTestnetAddr(self, addr):
+		if self.currency == 'btc':
+			return addr.startswith('t') \
+				or addr.startswith('m') \
+				or addr.startswith('n') \
+				or addr.startswith('2') \
+				or addr.startswith('tb1') \
+				or addr.startswith('bcrt1')
+		elif self.currency == 'bch':
+			return addr.startswith('bchtest:') \
+				or addr.startswith('bchreg:') \
+				or (not ':' in addr)	# prefixes may be stripped from BCH addresses
+		else:
+			return False
+
+	def Send(self, s, addr, amt):
+		if self.IsBitcoind():
+			return do_rpc(s, self, 'sendtoaddress', (addr, amt))
+		elif self.IsElectrum():
+			params = {}
+			params['amount'] = amt + 1e-8 # Electrum seems to have a rounding problem, so pay 1 extra satoshi just to make sure
+			params['destination'] = addr
+			if len(self.walpwd):
+				params['password'] = self.walpwd
+			tx = do_rpc(s, self, 'payto', params, needs_wallet=True)
+			#pprint.pprint(tx)
+			if not tx:
+				return None
+			if 'hex' in tx:
+				tx = tx['hex']
+			txid = do_rpc(s, self, 'broadcast', (tx, ))
+			#pprint.pprint(txid)
+			#print type(txid)
+			if isinstance(txid, (list, tuple)):
+				if len(txid) < 2 or txid[0] is not True:
+					print('error attempting to make payment: unexpected result ')
+					pprint.pprint(txid)
+					return None
+				txid = str(txid[1])
+			if not isinstance(txid, (str, unicode)):
+				print('error attempting to make payment: value for txid is not a string')
+				pprint.pprint(txid)
+				return None
+			return str(txid)
+		else:
+			return self.InvalidTypeError()
+
+	# finds transaction sent to an address
+	def FindPayment(self, s, addr, mintime, maxconf):
+		#print int(time.time()),'FindPayment', addr, mintime, maxconf
+		if self.IsBitcoind():
+			offset = 0
+			count = 20
+			while True:
+				r = do_rpc(s, self, 'listtransactions', ('*', count, offset))
+				if r is None:
+					print('error retrieving payment info for foreign address', addr)
+					return None
+				if not len(r):
+					#print 'FindPayment found no txs at', offset
+					return None
+				offset += count - 4
+				for e in r:
+					#pprint.pprint(e)
+					if e['time'] < mintime:
+						#print 'FindPayment past mintime at', offset
+						return None
+					if e['category'] != 'send':
+						continue
+					if e['abandoned']:
+						continue
+					if e['address'] == addr:
+						return e['txid']
+		elif self.IsElectrum():
+			r = do_rpc(s, self, 'getinfo')
+			if not 'blockchain_height' in (r or ()):
+				print('blockchain_height missing from getinfo')
+				print(r)
+				return None
+			height = r['blockchain_height']
+			r = do_rpc(s, self, 'getaddresshistory', (addr, ))
+			#print 'height', height
+			#pprint.pprint(r)
+			for t in (r or ()):
+				if not 'tx_hash' in t or not 'height' in t:
+					print('no tx_hash or height in getaddresshistory')
+					print(t)
+					continue
+				if t['height'] == 0 or height - t['height'] < maxconf:
+					return str(t['tx_hash'])
+		else:
+			return self.InvalidTypeError()
+
+	# retrieves transaction 'confirmations' and 'blockheight'
+	def GetTx(self, s, num, addr, txid, conf):
+		#print int(time.time()),'GetTx', addr, num, txid
+		if self.IsBitcoind():
+			r = do_rpc(s, self, 'gettransaction', (txid, ))
+			if r is None:
+				print('error retrieving transaction info for payment of match', num, 'txid', txid)
+				return None
+			#pprint.pprint(r)
+			if not 'confirmations' in r:
+				print('no confirmations for payment of match', num, 'txid', txid)
+				print(r)
+				return None
+			if r['confirmations'] < conf:
+				return r
+			if 'blockheight' in r:
+				return r
+			if not 'blockhash' in r:
+				print('no blockheight or blockhash for payment of match', num, 'txid', txid)
+				print(r)
+				return None
+			e = do_rpc(s, self, 'getblockheader', (r['blockhash'], ))
+			#pprint.pprint(e)
+			if not 'height' in (e or ()):
+				print('no height for payment of match', num, 'txid', txid, 'blockhash', r['blockhash'])
+				print(e)
+				return None
+			r['blockheight'] = e['height']
+			return r
+		elif self.IsElectrum():
+			r = do_rpc(s, self, 'getaddresshistory', (addr, ))
+			#pprint.pprint(r)
+			c = None
+			for t in (r or ()):
+				if not 'tx_hash' in t or not 'height' in t:
+					print('no tx_hash or height for payment of match', num, 'address', addr, 'txid', txid)
+					print(t)
+					return None
+				if txid == str(t['tx_hash']) and t['height'] and (not c or c['height'] > t['height']):
+					c = t
+			if c is None:
+				return None
+			r = do_rpc(s, self, 'getinfo')
+			if not 'blockchain_height' in (r or ()):
+				print('no blockchain_height for payment of match', num, 'txid', txid)
+				print(r)
+				return None
+			c['blockheight'] = c['height']
+			c['confirmations'] = r['blockchain_height'] - c['blockheight'] + 1
+			return c
+		else:
+			return self.InvalidTypeError()
+
+	def AbandonTx(self, s, txid):
+		if self.IsBitcoind():
+			return do_rpc(s, self, 'abandontransaction', (txid, ))
+		elif self.IsElectrum():
+			return False #do_rpc(s, self, 'removelocaltx', (txid, ))
+		else:
+			return self.InvalidTypeError()
+
+def check_if_testnet(s):
+	global IsTestnet
+	print('CredaCash rpc port:', Creda.port)
+	r = do_rpc(s, Creda, 'getinfo')
+	#print r
+	if not r:
+		print('ERROR querying CredaCash wallet at port', Creda.port)
+		exit()
+	try:
+		if r['connections'] < 1:
+			print('ERROR: ccnode is not yet synced to the CredaCash network')
+			exit()
+		IsTestnet = r['testnet']
+	except KeyError:
+		print('ERROR: unable to determine if the CredaCash wallet is connected to a testnet')
+		print(r)
+		exit()
+	print('IsTestnet', IsTestnet)
+
+def init_bitcoind(s):
+	if Foreign.walname is not None:
+		walinfo = do_rpc(s, Foreign, 'getwalletinfo')
+		if walinfo and 'walletname' in walinfo and walinfo['walletname'] == Foreign.walname:
+			print(FOREIGN, 'wallet', Foreign.walname, 'already loaded')
+		else:
+			print('Loading', FOREIGN.lower(), 'wallet', Foreign.walname)
+			do_rpc(s, Foreign, 'loadwallet', (Foreign.walname, ))
+
+stop_electrum = False
+
+def init_electrum(s):
+	# Determine Electrum exe name
+	global Foreign, stop_electrum
+	Foreign.exefile = os.path.expandvars(os.path.expanduser(Foreign.exefile))
+	if not os.path.isfile(Foreign.exefile):
+		print('ERROR:', ELECTRUM, 'executable file not found: "%s"' % Foreign.exefile)
+		exit()
+	print(ELECTRUM, 'executable:', Foreign.exefile)
+	Foreign.launch = [Foreign.exefile]
+	Foreign.launch.extend(Foreign.exeopts.split())
+	#pprint.pprint(Foreign.launch)
+
+	if Foreign.currency == 'btc':
+		query = Foreign.launch + ['--offline']
+	else:
+		query = Foreign.launch
+
+	# Set rpcsock = tcp if the electrum default is unix
+	import socket
+	import subprocess
+	if hasattr(socket, 'AF_UNIX'):
+		print('setting', ELECTRUM, 'rpc mode...')
+		subprocess.Popen(query + ['setconfig', 'rpcsock', 'tcp']).wait()
+
+	# Launch Electrum in GUI or daemon mode
+	if Foreign.exemode == 'daemon':
+		if not hasattr(os,'fork'):
+			print('ERROR: daemon not supported on systems that lack os.fork()')
+			exit()
+		print('starting', ELECTRUM, 'daemon...')
+		stop_electrum = True
+		if Foreign.currency == 'bch':
+			subprocess.Popen(Foreign.launch + ['daemon', 'start'])
+		else:
+			subprocess.Popen(Foreign.launch + ['daemon', '-d'])
+	else:
+		print('starting %s...' % ELECTRUM)
+		subprocess.Popen(Foreign.launch)
+	time.sleep(10)
+
+	# Determine Electrum data path
+	# TODO: better error checking/reporting here:
+	print('querying %s...' % ELECTRUM)
+	r = subprocess.check_output(Foreign.launch + ['getinfo'])
+	#pprint.pprint(r)
+	try:
+		j = json.loads(makestring(r))
+		p = j['path']
+	except:
+		p = ''
+	if not len(p):
+		print('ERROR: unable to obtain', ELECTRUM, 'data path')
+		raise SystemExit()
+	print(ELECTRUM, 'data path: "%s"' % p)
+
+	# Determine Electrum RPC port, username and password
+	try:
+		f = open(p + '/daemon')
+		l = f.read()
+		f.close()
+	except:
+		print('ERROR: unable to read', ELECTRUM, 'daemon file')
+		raise SystemExit()
+	try:
+		c = l.split(')', 1)[0].strip('(').split(',')
+		Foreign.rpcsock = c[0].strip().strip("'")
+		Foreign.port = c[-1].strip().strip("'")
+		#if Foreign.port.isdigit():
+		Foreign.port = int(Foreign.port)
+		#print Foreign.rpcsock, Foreign.port
+	except:
+		print('ERROR: unable to parse', ELECTRUM, 'port from string "%s"' % l)
+		if Foreign.rpcsock == 'unix':
+			print('Unix domain sockets are not supported')
+			print('Try completely closing', ELECTRUM, 'and re-running this script')
+		raise SystemExit()
+	print(ELECTRUM, 'rpc port:', Foreign.port)
+	Foreign.user = str(subprocess.check_output(Foreign.launch + ['getconfig', 'rpcuser']).decode()).strip()
+	print(ELECTRUM, 'rpc user:', Foreign.user)
+	Foreign.pwd = str(subprocess.check_output(Foreign.launch + ['getconfig', 'rpcpassword']).decode()).strip()
+	if len(Foreign.pwd):
+		print(ELECTRUM, 'rpc password: %c*****' % Foreign.pwd[0])
+		#print Foreign.pwd
+
+	# Check if Electrum RPC works
+	r = do_rpc(s, Foreign, 'version')
+	if not r:
+		print('ERROR: unable to get', ELECTRUM, 'version')
+		raise SystemExit()
+	print(ELECTRUM, 'version', r)
+
+	# Determine Electrum wallet path and password
+	walp = {}
+	if not len(Foreign.walname):
+		Foreign.walname = 'default_wallet'
+	walfile = Foreign.waldir
+	if not len(walfile):
+		walfile = p + '/wallets/'
+	walfile += Foreign.walname
+	walfile = walfile.replace('\\\\', '/')
+	walfile = walfile.replace('\\', '/')
+	walp['wallet_path'] = walfile
+	Foreign.walpath = walfile
+	print(ELECTRUM, 'wallet file: "%s"' % walfile)
+	if len(Foreign.walpwd):
+		walp['password'] = Foreign.walpwd
+		print(ELECTRUM, 'wallet password: %c*****' % Foreign.walpwd[0])
+		#print walp['password']
+
+	# Load Electrum wallet
+	print('loading', ELECTRUM, 'wallet...')
+	if Foreign.currency != 'bch':
+		#pprint.pprint(walp)
+		r = do_rpc(s, Foreign, 'load_wallet', walp)
+		if not r:
+			print('ERROR: unable to load', ELECTRUM, 'wallet')
+			raise SystemExit()
+	else:
+		#pprint.pprint(Foreign.launch)
+		sp = subprocess.Popen(Foreign.launch + ['--wallet', Foreign.walpath, '--walletpassword', '-', 'daemon', 'load_wallet'], bufsize=4000, stdin=subprocess.PIPE, stdout=subprocess.PIPE) # must have stdin and stdout for wallet password injection to work
+		sp.stdin.write(Foreign.walpwd.encode())
+		sp.stdin.close()
+		for i in range(60):
+			time.sleep(1)
+			if sp.poll() is not None:
+				break
+		if sp.poll() is None:
+			sp.kill()
+		rc = sp.wait()
+		if rc:
+			print('ERROR:', ELECTRUM, 'returned status code', rc)
+			raise SystemExit()
